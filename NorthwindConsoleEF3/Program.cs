ed8cc599a3d7524c48f44bf1e2ba429f0a63e898
Carlos Pivotto <carlospivotto@msn.com>
2020-05-16 17:08:52 -0300

    Adicionar e Listar Produtos.

--- NorthwindConsoleEF3/Program.cs
+++ NorthwindConsoleEF3/Program.cs
@@ -11,7 +11,19 @@ namespace NorthwindConsoleEF3
     {
         static void Main(string[] args)
         {
-            ConsultarProdutos();
+            ListarProdutos();
+            WriteLine("");
+            WriteLine("");
+
+            if(AdicionarProduto("Geladeira Brastemp 270L", 1499M, 2))
+            {
+                WriteLine("Produto adicionado com sucesso!");
+            }
+
+            WriteLine("");
+            WriteLine("");
+            ListarProdutos();
+
         }
 
         private static void ConsultarCategorias()
@@ -52,6 +64,34 @@ namespace NorthwindConsoleEF3
             foreach (var p in produtos)
             {
                 WriteLine("{0}: {1} custa {2:R$#,##0.00} e possui {3} unidades em estoque.", p.ProdutoId, p.Nome, p.Preco, p.Estoque);
+            }
+        }
+
+        private static bool AdicionarProduto(string nomeProduto, decimal? preco, int categoriaId)
+        {
+            using var db = new NorthwindDb();
+            var produto = new Produto
+            {
+                Nome = nomeProduto,
+                Preco = preco,
+                CategoriaId = categoriaId
+            };
+
+            // Marcar este produto como ADICIONADO no change tracker:
+            db.Produtos.Add(produto);
+            // Persistir mudanças no BD:
+            int affected = db.SaveChanges();
+            return (affected == 1);
+        }
+
+        private static void ListarProdutos()
+        {
+            using var db = new NorthwindDb();
+            WriteLine("{0,-3} {1,-40} {2,10} {3,5} {4}", "ID", "Produto", "Preço", "Estoque", "Descontinuado");
+            var produtos = db.Produtos.OrderBy(p => p.Nome);
+            foreach(var p in produtos)
+            {
+                WriteLine("{0:000} {1,-40} {2,10:R$#,##0.00} {3:00000} {4}", p.ProdutoId, p.Nome, p.Preco, p.Estoque, p.Descontinuado);
             }
         }
     }
