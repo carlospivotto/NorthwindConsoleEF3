c156671144d905457df14161bebfcd9bedb682dc
Carlos Pivotto <carlospivotto@msn.com>
2020-05-16 16:02:17 -0300

    Consultar Categorias.

--- NorthwindConsoleEF3/Program.cs
+++ NorthwindConsoleEF3/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindConsoleEF3.Modelos;
+using System;
+using System.Linq;
+using static System.Console;
 
 namespace NorthwindConsoleEF3
 {
@@ -6,7 +10,24 @@ namespace NorthwindConsoleEF3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsultarCategorias();
+        }
+
+        private static void ConsultarCategorias()
+        {
+            using var db = new NorthwindDb();
+            WriteLine("Categorias e quantos produtos possuem:");
+            //Uma consulta que recupera todas as categorias
+            //e, para cada categoria, seus produtos relacionados:
+            IQueryable<Categoria> categorias = db.Categorias.Include(c => c.Produtos);
+            foreach (var c in categorias)
+            {
+                WriteLine($"{c.Nome} possui {c.Produtos.Count} produtos.");
+                foreach (var p in c.Produtos)
+                {
+                    WriteLine($" — {p.Nome} ({p.Estoque} unidades no estoque)");
+                }
+            }
         }
     }
 }
